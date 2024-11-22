cccec945e6ce3d96daa7c118be195d30e9a64ee5
Chung <xc.cx.chung@gmail.com>
2024-11-22 13:40:02 -0500

    pictures added

--- ICS3U1 - Assignment 5/src/components/Feature.vue
+++ ICS3U1 - Assignment 5/src/components/Feature.vue
@@ -6,15 +6,15 @@
     <h2>Featured Movies</h2>
         <div class="movie-grid">
             <div class="movie-card">
-                <img src="pictures/moneyball.jpg">
+                <img src="../assets/moneyball.jpg">
                 <p>Moneyball</p>
             </div>
             <div class="movie-card">
-                <img src="pictures/transformers.jpg">
+                <img src="../assets/transformers.jpg">
                 <p>Transformers</p>
             </div>
             <div class="movie-card">
-                <img src="pictures/oppenheimer.jpg">
+                <img src="../assets/oppenheimer.jpg">
                 <p>Oppenheimer</p>
             </div>
         </div>
